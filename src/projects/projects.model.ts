694fa9e16480510c4e5a67446ae9481f63f3f038
carefreeav09 <carefreeav09@gmail.com>
2023-08-10 18:43:24 +0000

    added appType as a required field

--- src/projects/projects.model.ts
+++ src/projects/projects.model.ts
@@ -1,33 +1,37 @@
 import mongoose from "mongoose";
 
 const Project = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        min: 3,
-        max: 255
-    },
-    description: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 1024
-    },
-    technologies: {
-        type: [String],
-        required: true,
-        min: 6,
-        max: 1024
-    },
-    freelance: Boolean,
-    thumbnail: String,
-    images: [String],
-    url: {
-        type: String,
-        required: false,
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 255
+  },
+  description: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 1024
+  },
+  technologies: {
+    type: [String],
+    required: true,
+    min: 6,
+    max: 1024
+  },
+  freelance: Boolean,
+  thumbnail: String,
+  images: [String],
+  url: {
+    type: String,
+    required: false,
 
-    },
-    github: String
+  },
+  github: String,
+  appType: {
+    type: String,
+    required: true
+  }
 });
 
 export default mongoose.model("Project", Project);
