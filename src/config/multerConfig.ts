2384c465aa8edc17e4488791fd6323bbbc9f5fe3
Abhushan Gautam <carefreeav09@gmail.com>
2023-08-25 20:40:46 +0545

    feat: configured cloudinary

--- src/config/multerConfig.ts
+++ src/config/multerConfig.ts
@@ -1,6 +1,11 @@
 import multer from 'multer';
 import fs from 'fs';
 import express from 'express';
+import cloudinary from 'cloudinary';
+
+
+const cloudinaryV2 = cloudinary.v2;
+
 
 interface IFileUploader {
     upload(filter: any): multer.Multer | any;
@@ -8,7 +13,19 @@ interface IFileUploader {
 
 class FileUploader implements IFileUploader {
     private storage = multer.diskStorage({
-        destination: (_req, _file, cb) => {
+        destination: async (_req, _file, cb) => {
+
+            try {
+                const result = await cloudinaryV2.uploader.upload(_file.path, (error, result) => {
+                    console.log(error, 'rrors');
+                });
+
+                console.log(result, 'result')
+            }
+            catch (error) {
+                console.log(error, 'error inside the catch block');
+            }
+
             const path = `./uploads`;
             fs.mkdirSync(path, { recursive: true });
             cb(null, path);
