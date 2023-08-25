a0a80b2ffedfc00a5ca014cd38943ad514368df7
Abhushan Gautam <carefreeav09@gmail.com>
2023-08-26 00:52:18 +0545

    fix: cloudinary now works

--- src/config/multerConfig.ts
+++ src/config/multerConfig.ts
@@ -1,56 +1,26 @@
-import multer from 'multer';
-import fs from 'fs';
-import cloudinary from 'cloudinary';
-import dotenv from 'dotenv';
+import multer from "multer";
+import dotenv from "dotenv";
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
 
 dotenv.config();
 
-const cloudinaryV2 = cloudinary.v2;
-
-cloudinaryV2.config({
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'products',
+        // format: async (_req: any, file: any) => 'png',
+        // public_id: (req: any, file: any) => 'computed-filename-using-request',
+    },
+});
 
-interface IFileUploader {
-    upload(filter: any): multer.Multer | any;
-}
-
-class FileUploader implements IFileUploader {
-    private storage = multer.diskStorage({
-        destination: async (_req, _file, cb) => {
-            try {
-                //@ts-ignore
-                const result = await cloudinaryV2.uploader.upload(_file.path, (error, result) => {
-                    console.log(error, 'rrors');
-                });
-
-                console.log(result, 'result')
-            }
-            catch (error) {
-                console.log(error, 'error inside the catch block');
-            }
-
-            const path = `./uploads`;
-            fs.mkdirSync(path, { recursive: true });
-            cb(null, path);
-        },
-        filename: (_req, file, cb) => {
-            cb(null, `${file.originalname.split(' ').join('-')}`);
-        },
-    });
-
-    upload(filter: any) {
-        return multer({ storage: this.storage, fileFilter: filter });
-    }
-}
-
-
-//
 export const upload = (filter: any = null): multer.Multer => {
-    return new FileUploader().upload(filter);
+    return multer({ storage: storage, fileFilter: filter });
 };
-
-
