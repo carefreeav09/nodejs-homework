ebed35797c2387024ddc9923393d572ee8d856c4
carefreeav09 <carefreeav09@gmail.com>
2023-12-11 08:36:25 +0000

    Fixed lint issues.

--- src/config/multerConfig.ts
+++ src/config/multerConfig.ts
@@ -7,22 +7,22 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: 'products',
-        // format: async (_req: any, file: any) => 'png',
-        // public_id: (req: any, file: any) => 'computed-filename-using-request',
-    },
+  cloudinary: cloudinary,
+  params: {
+    folder: 'products',
+    // format: async (_req: any, file: any) => 'png',
+    // public_id: (req: any, file: any) => 'computed-filename-using-request',
+  },
 
 });
 
 export const upload = (filter: any = null) => {
-    return multer({ storage: storage, fileFilter: filter });
+  return multer({ storage: storage, fileFilter: filter });
 
 };
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -2,51 +2,51 @@ import { IProjects } from "./project.types";
 import { IProjectsBusiness } from "./projects.business";
 
 interface IProjectService {
-    create: (data: IProjects, files: {
-        [fieldname: string]: Express.Multer.File[];
-    } | Express.Multer.File[] | undefined) => Promise<IProjects>;
-    getAllProjects: () => Promise<IProjects[]>;
-    getProjectById: (id: string) => Promise<IProjects | null>;
+  create: (data: IProjects, files: {
+    [fieldname: string]: Express.Multer.File[];
+  } | Express.Multer.File[] | undefined) => Promise<IProjects>;
+  getAllProjects: () => Promise<IProjects[]>;
+  getProjectById: (id: string) => Promise<IProjects | null>;
 }
 
 class ProjectService implements IProjectService {
-    private projectModel;
-    private projectBusiness;
+  private projectModel;
+  private projectBusiness;
 
-    constructor(
-        protected modal: any,
-        protected business: IProjectsBusiness,
-    ) {
-        this.projectModel = modal;
-        this.projectBusiness = business;
+  constructor(
+    protected modal: any,
+    protected business: IProjectsBusiness,
+  ) {
+    this.projectModel = modal;
+    this.projectBusiness = business;
+  }
+
+  async create(data: IProjects, files: {
+    [fieldname: string]: Express.Multer.File[];
+  } | Express.Multer.File[] | undefined) {
+    const thumbnail = files && "thumbnail" in files && this.projectBusiness.getThumbnailUrl(files.thumbnail);
+    const images = files && "images" in files && this.projectBusiness.getImagesUrl(files.images);
+
+    console.log(thumbnail, images, 'thumbnail and images');
+
+    const newData = {
+      ...data,
+      thumbnail,
+      images
     }
+    const project = await this.projectModel.create(newData);
+    return project;
+  }
 
-    async create(data: IProjects, files: {
-        [fieldname: string]: Express.Multer.File[];
-    } | Express.Multer.File[] | undefined) {
-        const thumbnail = files && "thumbnail" in files && this.projectBusiness.getThumbnailUrl(files.thumbnail);
-        const images = files && "images" in files && this.projectBusiness.getImagesUrl(files.images);
+  async getAllProjects() {
+    const projects = await this.projectModel.find();
+    return projects;
+  }
 
-        console.log(thumbnail, images, 'thumbnail and images');
-
-        const newData = {
-            ...data,
-            thumbnail,
-            images
-        }
-        const project = await this.projectModel.create(newData);
-        return project;
-    }
-
-    async getAllProjects() {
-        const projects = await this.projectModel.find();
-        return projects;
-    }
-
-    async getProjectById(id: string) {
-        const project = await this.projectModel.findById(id);
-        return project;
-    }
+  async getProjectById(id: string) {
+    const project = await this.projectModel.findById(id);
+    return project;
+  }
 }
 
 export default ProjectService;
